9c977761d41b48ba5a7a9adb92979c4f3bb4fa6a
Rafael Alves <rafaell.oliveira001@gmail.com>
2023-07-03 11:44:02 -0300

    Correção para que quando redimensione a tela mude o componente de desktopxmobile

--- src/components/main/section_produtos/Produtos.jsx
+++ src/components/main/section_produtos/Produtos.jsx
@@ -1,36 +1,97 @@
+import React, { useState, useEffect } from "react";
 import Titulo from "../common/Titulo";
-import titulosDescricoes from "../../../js/titulosDescricoes.js"
+import titulosDescricoes from "../../../js/titulosDescricoes.js";
 import Card from "../common/ImgCard";
-import BasketBall from "../../../img/tenis_basketball.png"
-import LifeStyle from "../../../img/tenis_life_style.png"
-import Performace from "../../../img/tenis_performace.png"
-import Camisetas from "../../../img/camisetas.png"
-import Moletons from "../../../img/moletons.png"
-import Shorts from "../../../img/shorts.png"
-import Bones from "../../../img/bones.png"
-import Meias from "../../../img/meias.png"
-import Faixas from "../../../img/faixas.png"
+import BasketBall from "../../../img/tenis_basketball.png";
+import LifeStyle from "../../../img/tenis_life_style.png";
+import Performace from "../../../img/tenis_performace.png";
+import Camisetas from "../../../img/camisetas.png";
+import Moletons from "../../../img/moletons.png";
+import Shorts from "../../../img/shorts.png";
+import Bones from "../../../img/bones.png";
+import Meias from "../../../img/meias.png";
+import Faixas from "../../../img/faixas.png";
 import Titulo3 from "../common/Titulo3";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination, Mousewheel, Keyboard, Scrollbar, A11y, Controller } from "swiper";
-import { useState, useEffect } from "react";
-
-
-//IMPLEMENTAR IMPORT JSON COM MAP
+import { Navigation, Pagination, Scrollbar, A11y, Controller } from "swiper";
 
 export default function Produtos() {
+    const [produtos, setProdutos] = useState("");
 
-    const [produtos, setProdutos] = useState('')
-    // const [produtosJSON, setProdutosJSON] = useState([])
+    const produtosJSON = {
+        tenis: [
+            {
+                title: "Tênis de Basketball",
+                image: BasketBall,
+                category: "Basketball",
+            },
+            {
+                title: "Tênis Lifestyle",
+                image: LifeStyle,
+                category: "LifeStyle",
+            },
+            {
+                title: "Tênis de Performance",
+                image: Performace,
+                category: "Performace",
+            },
+        ],
+        diaadia: [
+            {
+                title: "Camisetas",
+                image: Camisetas,
+                category: "Camisetas",
+            },
+            {
+                title: "Moletons",
+                image: Moletons,
+                category: "Moletons",
+            },
+            {
+                title: "Shorts",
+                image: Shorts,
+                category: "Shorts",
+            },
+        ],
+        acessorios: [
+            {
+                title: "Bonés",
+                image: Bones,
+                category: "Bonês",
+            },
+            {
+                title: "Meias",
+                image: Meias,
+                category: "Meias",
+            },
+            {
+                title: "Faixas",
+                image: Faixas,
+                category: "Faixas",
+            },
+        ],
+    };
 
     useEffect(() => {
-        
-        (window.innerWidth < 800 ? mobile() : desktop())
+        handleResize(); 
 
-    },[])
+        window.addEventListener("resize", handleResize); 
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    function handleResize() {
+        if (window.innerWidth < 800) {
+            mobile();
+        } else {
+            desktop();
+        }
+    }
 
     function mobile() {
         setProdutos(
@@ -41,29 +102,16 @@ export default function Produtos() {
                     slidesPerView={1}
                     navigation={true}
                     scrollbar={true}
-                    pagination={true}>
-
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Tênis"} />
-                            <Card image={BasketBall} categoryName={"Basketball"} />
-
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Tênis"} />
-
-                            <Card image={LifeStyle} categoryName={"LifeStyle"} />
-
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Tênis"} />
-                            <Card image={Performace} categoryName={"Performace"} />
-                        </div>
-                    </SwiperSlide>
+                    pagination={true}
+                >
+                    {produtosJSON.tenis.map((produto, index) => (
+                        <SwiperSlide key={index}>
+                            <div>
+                                <Titulo3 titulo={produto.title} />
+                                <Card image={produto.image} categoryName={produto.category} />
+                            </div>
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
 
                 <Swiper
@@ -72,28 +120,16 @@ export default function Produtos() {
                     slidesPerView={1}
                     navigation={true}
                     scrollbar={true}
-                    pagination={true}>
-
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Para o dia a dia"} />
-                            <Card image={Camisetas} categoryName={"Camisetas"} />
-
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Para o dia a dia"} />
-                            <Card image={Moletons} categoryName={"Moletons"} />
-
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Para o dia a dia"} />
-                            <Card image={Shorts} categoryName={"Shorts"} />
-                        </div>
-                    </SwiperSlide>
+                    pagination={true}
+                >
+                    {produtosJSON.diaadia.map((produto, index) => (
+                        <SwiperSlide key={index}>
+                            <div>
+                                <Titulo3 titulo={produto.title} />
+                                <Card image={produto.image} categoryName={produto.category} />
+                            </div>
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
 
                 <Swiper
@@ -102,71 +138,50 @@ export default function Produtos() {
                     slidesPerView={1}
                     navigation={true}
                     scrollbar={true}
-                    pagination={true}>
-
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Acessórios"} />
-                            <Card image={Bones} categoryName={"Bonês"} />
-
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Acessórios"} />
-                            <Card image={Meias} categoryName={"Meias"} />
-
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div>
-                            <Titulo3 titulo={"Acessórios"} />
-                            <Card image={Faixas} categoryName={"Faixas"} />
-                        </div>
-                    </SwiperSlide>
+                    pagination={true}
+                >
+                    {produtosJSON.acessorios.map((produto, index) => (
+                        <SwiperSlide key={index}>
+                            <div>
+                                <Titulo3 titulo={produto.title} />
+                                <Card image={produto.image} categoryName={produto.category} />
+                            </div>
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
-
             </>
-        )
-
+        );
     }
 
     function desktop() {
-
-        // setProdutos(produtosJSON.map((prod, index) =>{
-        //     <Card key={index} image={prod.image} categoryName={prod.name}/>
-        // }))
         setProdutos(
             <>
                 <div>
                     <Titulo3 titulo={"Tênis"} />
-
-                    <Card image={BasketBall} categoryName={"Basketball"} />
-                    <Card image={LifeStyle} categoryName={"LifeStyle"} />
-                    <Card image={Performace} categoryName={"Performace"} />
+                    {produtosJSON.tenis.map((produto, index) => (
+                        <Card key={index} image={produto.image} categoryName={produto.category} />
+                    ))}
                 </div>
                 <div>
                     <Titulo3 titulo={"Para o dia a dia"} />
-                    <Card image={Camisetas} categoryName={"Camisetas"} />
-                    <Card image={Moletons} categoryName={"Moletons"} />
-                    <Card image={Shorts} categoryName={"Shorts"} />
+                    {produtosJSON.diaadia.map((produto, index) => (
+                        <Card key={index} image={produto.image} categoryName={produto.category} />
+                    ))}
                 </div>
                 <div>
                     <Titulo3 titulo={"Acessórios"} />
-                    <Card image={Bones} categoryName={"Bonês"} />
-                    <Card image={Meias} categoryName={"Meias"} />
-                    <Card image={Faixas} categoryName={"Faixas"} />
+                    {produtosJSON.acessorios.map((produto, index) => (
+                        <Card key={index} image={produto.image} categoryName={produto.category} />
+                    ))}
                 </div>
             </>
-        )
+        );
     }
 
     return (
         <section id="produtos" className="sectionMarge">
             <Titulo titulo={titulosDescricoes["produtos"].titulo} />
-            <div className="cards">
-                {produtos}
-            </div>
+            <div className="cards">{produtos}</div>
         </section>
-    )
+    );
 }
