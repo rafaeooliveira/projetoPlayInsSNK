011c2cc4c2ea083ae81d68d830b5d165151e5331
Rafael Alves <rafaell.oliveira001@gmail.com>
2023-07-08 10:11:45 -0300

    Corrigindo btn whatsapp

--- src/components/tools/Whatsapp.jsx
+++ src/components/tools/Whatsapp.jsx
@@ -4,6 +4,16 @@ import {FloatingWhatsApp} from 'react-floating-whatsapp'
 
 export default function Whatsapp(){
     return(
-        <FloatingWhatsApp accountName="Play in's SNK" avatar="./img/logo.png" statusMessage='Online' chatMessage="Olá, seja bem vindo ao Play in's SNK, como posso ajudar?" placeholder='Digite sua mensagem' chatboxHeight={360} allowEsc allowClickAway darkMode phoneNumber='+5531971376984'/>
+        <FloatingWhatsApp 
+            accountName="Play in's SNK" 
+            avatar="./img/logo.png" 
+            statusMessage='Online' 
+            chatMessage="Olá, seja bem vindo ao Play in's SNK, como posso ajudar?" 
+            placeholder='Digite sua mensagem' 
+            phoneNumber='+5531971376984' 
+            darkMode
+            notification
+            notificationDelay={5}
+            />
     )
 }
