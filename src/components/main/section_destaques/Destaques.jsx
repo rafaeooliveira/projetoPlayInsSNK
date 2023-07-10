e767c9ff0dc53b6982aea635abd1c3ee8dbb2e6d
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2023-07-09 21:46:02 -0300

    Ajuste na seção DESTAQUES, adicionado imagens, nome e valores

--- src/components/main/section_destaques/Destaques.jsx
+++ src/components/main/section_destaques/Destaques.jsx
@@ -63,39 +63,63 @@ export default function Destaques() {
         },
         {
             id: 4,
-            imagem: "./img/destaque1.png",
-            titulo: "NIKE ZOOM FREAK 4",
-            valor: "R$1.899,99"
+            imagem: "./img/destaque4.png",
+            titulo: "NIKE ZION 2 (MOVE IN SILENCE)",
+            valor: "R$999,99"
         },
         {
             id: 5,
-            imagem: "./img/destaque2.png",
-            titulo: "AIR PENNY 2 x Stüssy",
-            valor: "R$1.599,99"
+            imagem: "./img/destaque5.png",
+            titulo: "AIR JORDAN LEGACY 312 LOW",
+            valor: "R$1.399,99"
         },
         {
             id: 6,
-            imagem: "./img/destaque3.png",
-            titulo: "AIR JORDAN 13",
-            valor: "R$1.899,99"
+            imagem: "./img/destaque6.png",
+            titulo: "TATUM 1 (BARBESHOP)",
+            valor: "R$999,99"
         },
         {
             id: 7,
-            imagem: "./img/destaque1.png",
-            titulo: "NIKE ZOOM FREAK 4",
-            valor: "R$1.899,99"
+            imagem: "./img/destaque7.png",
+            titulo: "REGATA GOLDEN STATE WARRIORS ",
+            valor: "R$649,99"
         },
         {
             id: 8,
-            imagem: "./img/destaque2.png",
-            titulo: "AIR PENNY 2 x Stüssy",
+            imagem: "./img/destaque8.png",
+            titulo: "CURRY FLOW 10 ",
             valor: "R$1.599,99"
         },
         {
             id: 9,
-            imagem: "./img/destaque3.png",
-            titulo: "AIR JORDAN 13",
-            valor: "R$1.899,99"
+            imagem: "./img/destaque9.png",
+            titulo: "JORDAN STADIUM 90(SAIL)",
+            valor: "R$1.199,99"
+        },
+        {
+            id: 10,
+            imagem: "./img/destaque10.png",
+            titulo: "BONÉ BOSTON CELTICS",
+            valor: "R$149,99"
+        },
+        {
+            id: 11,
+            imagem: "./img/destaque11.png",
+            titulo: "AIR JORDAN FLIGHT ORIGIN 4",
+            valor: "R$1.299,99"
+        },
+        {
+            id: 12,
+            imagem: "./img/destaque12.png",
+            titulo: "NIKE JA MORANT 1 \"TRIVIA\"",
+            valor: "R$1.099,99"
+        },
+        {
+            id: 13,
+            imagem: "./img/destaque13.png",
+            titulo: "NIKE LEBRON XX (PINK)",
+            valor: "R$1.299,99"
         }
     ];
 
